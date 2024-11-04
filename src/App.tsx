9c65eb515569329641ab0d883b36747900e5f367
Daniel Solorzano <dsolo.prof@gmail.com>
2024-11-03 19:37:07 -0600

    Installed axios and created data.json.
    Created data.json for easier dynamic update of the site. Installed axios for retrieval of data.json.

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,39 @@ import Home from "./Components/Home/Home";
 import Projects from "./Components/Projects/Projects";
 import About from "./Components/About/About";
 import Error from "./Components/Error/Error";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { SiteData } from "./SiteData";
 
 function App() {
+    const [data, setData] = useState<SiteData | null>(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get("/data.json");
+                setData(response.data);
+            } catch (error) {
+                console.error("Couldn't fetch data: ", error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
     const router = createBrowserRouter([
         {
             path: "/",
-            element: <Home />,
+            element: <Home data={data} />,
             errorElement: <Error />,
         },
         {
             path: "/projects",
-            element: <Projects />,
+            element: <Projects data={data} />,
         },
         {
             path: "/about",
-            element: <About />,
+            element: <About data={data} />,
         },
     ]);
 
