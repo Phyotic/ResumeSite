971945bac6e39fcf5cea1357c1d4d1f6b59be9fb
Daniel Solorzano <dsolo.prof@gmail.com>
2024-11-03 19:45:09 -0600

    Changed Project component to Card component.
    Refactored Project component to Card component for resuability.

---
+++ src/Components/Card/Card.tsx
@@ -0,0 +1,25 @@
+import "./Card.css";
+import { Card as CardData } from "../../SiteData";
+
+type CardProps = {
+    card: CardData;
+};
+
+function Card({ card }: CardProps) {
+    return (
+        <section className="card-container">
+            <img
+                className="card-cover-image"
+                src={"../../../public/" + card.cover}
+                alt={card.header}
+            ></img>
+
+            <section className="card-content">
+                <h1>{card.header}</h1>
+                <h2>{card["sub-header"]}</h2>
+            </section>
+        </section>
+    );
+}
+
+export default Card;
